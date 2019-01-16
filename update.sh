e95d0adb75d9fa9b474c0375f6070e0e48d1a014
jevic <jie_071218@163.com>
2019-01-16 12:03:53 +0800

    2019-01-16 12:03:53

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-curl -X POST -H 'Content-Type: application/yaml' http://192.168.2.68:9090/api/v1/namespaces/default/pods -d "$(cat template_demo.yaml)"
-#curl -sL http://192.168.2.68:9090/api/v1/namespaces/default/pods -o /dev/null
+#curl -X POST -H 'Content-Type: application/yaml' http://192.168.2.68:9090/api/v1/namespaces/default/pods -d "$(cat template_demo.yaml)"
+curl -sL http://192.168.2.68:9090/api/v1/namespaces/default/pods -o /dev/null
 code=$?
-
+code=1
 echo ""
 echo "====================================="
-
+echo $code
 if [ $code != 0 ];then
    echo "\033[31m[部署失败....详情查看response 信息\033[0m"
    exit 100
