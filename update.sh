bd3c57782fe756c7f80886998eda67f1f5bc158e
jevic <jie_071218@163.com>
2019-01-16 11:03:12 +0800

    2019-01-16 11:03:12

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -X POST -H 'Content-Type: application/yaml' http://192.168.2.68:9090/api/v1/namespaces/default/pods -d "$(cat demo.yaml)"
+curl -X POST -H 'Content-Type: application/yaml' http://192.168.2.68:9090/api/v1/namespaces/default/pods -d "$(cat template_demo.yaml)"
